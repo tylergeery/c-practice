46a7bd5c27c6ae63b9ab379cd08d998fb24b5473
tylergeery <tyler.geery@yahoo.com>
2017-10-10 22:42:15 -0700

    feeling rusty

--- matrix/NxM.rs
+++ matrix/NxM.rs
@@ -1,12 +1,43 @@
 use std::{env, thread};
 
+const ROUTINE_COUNT: i32 = 3;
+
+fn usage(file: &String) {
+	println!("Usage: {} <operation>\n", file);
+}
+
 fn main() {
+	let n = [
+		[1, 2, 3],
+		[0, 5, 0],
+		[7, 7, 7]
+	];
+
+	let m = [
+		[2, 2, 4],
+		[0, 6, 3],
+		[3, 1, 1]
+	];
+
+	let result_matrix = [
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 0]
+	];
+
 	let args: Vec<String> = env::args().collect();
-		
+
 	if args.len() < 2 {
 		let ref file = &args[0];
 		usage(file);
 		return;
+	}
+
+	match &args[1] {
+		"add" => add(), // Do nothing
+		"subtract" => subtract(),
+		"multiple" => muliply(),
+		_ => { usage(&args[1]); return; }
 	}
 
 	let handle = thread::spawn(|| {
@@ -20,8 +51,4 @@ fn main() {
 	for j in 1..5 {
 		println!("printing {} from subsequent loop", j);
 	}
-}
-
-fn usage(file: &String) {
-	println!("Usage: {} <operation>\n", file);
 }
