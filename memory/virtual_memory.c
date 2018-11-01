49bcfce7b6d74cc62c6f571f3f07e8898860e959
tylergeery <tyler.geery@yahoo.com>
2018-10-31 19:12:30 -0700

    start virtual memory script

--- memory/virtual_memory.c
+++ memory/virtual_memory.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 
 #define PAGE_ENTRIES 256
 #define PAGE_SIZE 256
@@ -14,14 +16,25 @@ char* tlb[TLB_ENTRIES];
 
 void parseLogicalAddress(Address* address, char* raw)
 {
+    int hex = (int)strtol(raw, NULL, 16);
+
     address->raw = raw;
+    address->page_number = hex ^ 0xf0;
+    address->page_number = hex ^ 0x0f;
+}
 
-    // TODO: parse raw address string
+char* readData(FILE *fd) {
+    char buffer[PAGE_SIZE];
 
-    // TODO: set page number && offset
+    fseek(fd, rand() % (PAGE_SIZE * (PAGE_ENTRIES - 1)), SEEK_SET);
+    fread(buffer, PAGE_SIZE, 1, fd);
+
+    return buffer;
 }
 
 int main(int argc, char **argv) {
+    FILE *fd = fopen("BACKING_STORE.bin", "r");
+
     // TODO: get list of page references
 
     // TODO: loop through addresses
@@ -32,6 +45,10 @@ int main(int argc, char **argv) {
     // TODO: check if address is in tlb
 
     // TODO: if not in TLB, pull values from backing store
+    char *data = readData(fd);
+
+    // TODO: insert into TLB
 
 
+    fclose(fd);
 }
